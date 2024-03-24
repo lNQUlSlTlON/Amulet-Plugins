55aabd4ec03fc781be47b70788b6725bba5d35b3
The Inquisition <137798505+lNQUlSlTlON@users.noreply.github.com>
2024-03-23 18:42:21 -0600

    Update lantern_and_post_placer_v1.py

--- lantern_and_post_placer_v1.py
+++ lantern_and_post_placer_v1.py
@@ -1,3 +1,12 @@
+# Programmatic Lighting Placer
+# Place lighting on top of a post of your choice, or 
+#
+# Make sure your selection is only 1 block tall in the Y dimension
+#
+# (c) 2024 Black Forest Creations
+# Blame:  @lNQUlSlTlON
+
+
 import os
 import json
 import numpy as np
@@ -74,6 +83,12 @@ class Detect_Edges(wx.Panel, DefaultOperationUI):
 
         print("Operation completed successfully.")            
 
+"""
+These methods are built to place blocks of your choice under lighting of your choice,
+or just to place standing torches, spaced out by the radius of your choice.
+NumPy is used to build arrays for the spacing of the lights.
+"""
+    
     @staticmethod
     def read_selection(world: "BaseLevel", dimension: Dimension, selection: SelectionGroup):
         coordinates = Detect_Edges.get_coordinates(world, dimension, selection)
